830eba01173819159da78e8a887d5b4b514be043
onlyrads-psyche <onlyrads@gmail.com>
2025-09-20 08:16:55 +0000

    Add tourism project files

--- model_building/data_register.py
+++ model_building/data_register.py
@@ -6,6 +6,16 @@ import os
 repo_id = "dr-psych/tourism_project"
 repo_type = "dataset"
 
+# Stating HF token environment and stating the secret key
+HF_TOKEN = os.getenv("HF_TOKEN")
+if HF_TOKEN is None:
+    raise ValueError("Hugging Face token (HF_TOKEN) is not set as an environment variable.")
+
+from huggingface_hub import HfApi
+api = HfApi()
+# Pass the token explicitly when creating or interacting with repos
+api.create_repo(name="dr-psych/tourism_project", token=HF_TOKEN, repo_type="dataset")
+
 # Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
